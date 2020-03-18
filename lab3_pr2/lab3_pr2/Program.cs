7f7a6e471cfa18e2186fdb7c3904ae963476f6c5
Alina Timchenko <58236624+atmchnk@users.noreply.github.com>
2020-03-18 21:13:38 +0200

    Add files via upload

---
+++ lab3_pr2/lab3_pr2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace lab3_pr2
+{
+    class Constant
+    {
+        public const int N = 10;
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            int[] myArray = new int[Constant.N];
+            int i;
+            for (i = 0; i < Constant.N; i++)
+                myArray[i] = i * i;
+            for (i = 0; i < Constant.N; i++)
+                Console.WriteLine(myArray[i]);
+        }
+    }
+}
