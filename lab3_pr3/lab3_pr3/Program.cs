f0aadf572de07d26cbd2f3bb1ff1501260f401d3
Alina Timchenko <58236624+atmchnk@users.noreply.github.com>
2020-03-19 11:25:00 +0200

    Add files via upload

---
+++ lab3_pr3/lab3_pr3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace lab3_pr3
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            Console.Write("Введіть кількість елементі масиву: \t");
+
+            int elementsCount = int.Parse(Console.ReadLine());
+            int[] myArray = new int[elementsCount];
+            try
+            {
+                for (int i = 0; i < myArray.Length; i++)
+                {
+                    Console.Write($"Введіть елемент масиву під індексом {i}:\t ");
+                    myArray[i] = int.Parse(Console.ReadLine());
+                }
+
+                Console.WriteLine("Вивід масиву: ");
+                for(int i = 0; i < myArray.Length; i++)
+                {
+                    Console.WriteLine(myArray[i]);
+                }
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Вихід за межі діапазону");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Невірний формат вводу даних");
+            }
+            catch (OutOfMemoryException)
+            {
+                Console.WriteLine("Недостатньо пам'яті для створення нового об'єкта");
+            }
+        }
+    }
+}
