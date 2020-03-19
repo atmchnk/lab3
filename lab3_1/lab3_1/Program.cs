4422a613ee63ba18bed3ab41e71bd8567f0b18f6
Alina Timchenko <58236624+atmchnk@users.noreply.github.com>
2020-03-19 12:53:14 +0200

    Add files via upload

---
+++ lab3_1/lab3_1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace lab3_1
+{
+    class Constant
+    {
+        public const int N = 4;
+        public const int M = 5;
+        public const int L = 0;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            int[] A = new int[Constant.N] { 1, 2, 3, 4 };
+            int[] B = new int[Constant.M] { 5, 6, 7, 9, 10 };
+            int[] C = new int[Constant.L];
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (A[i] % 2 == 0)                       
+                Console.Write (A[i] + " "); //видалити парні елементи
+            }
+
+            for (int i = 0; i < B.Length; i++)
+            {
+                if (B[i] % 2 != 0)
+                    Console.Write(B[i] + " "); //видалити непарні елементи
+            }
+            
+            for (int i = 0; i < C.Length; i++)
+            {
+                C[i] = A[i] + B[i];               
+                Console.Write(C[i] + " "); //сформувати новий масив на сонові двох попередніх
+            }          
+        }
+    }
+}
